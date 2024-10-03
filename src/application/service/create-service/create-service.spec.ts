47608ad307189016364c8b9f5e9ccc5bbf25a44c
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-10-02 23:22:47 -0300

    test: add unit tests for CreateServiceUsecase error handling

--- src/application/service/create-service/create-service.spec.ts
+++ src/application/service/create-service/create-service.spec.ts
@@ -1,7 +1,9 @@
+import { InvalidParamError } from '../../../domain/exceptions/invalid-params-error';
 import { ServiceGateway } from '../../../domain/gateway/service.gateway';
+import { ServerError } from '../../exception/server-error';
 import { CreateServiceInputDto, CreateServiceUsecase } from './create-service.usecase';
 
-const mockServiceGateway: ServiceGateway = {
+const mockServiceGateway: jest.Mocked<ServiceGateway> = {
   save: jest.fn()
 };
 
@@ -29,5 +31,51 @@ describe('CreateServiceUsecase', () => {
 
     // Verifica se o ID não é nulo ou indefinido (assumindo que a lógica de criação de serviços gera um ID)
     expect(output.id).toBeDefined();
+  });
+
+  test('Deve lançar um erro se o nome do serviço estiver vazio', async () => {
+    const input: CreateServiceInputDto = {
+      name: '', // Nome vazio
+      description: 'Este é um serviço de teste',
+      icon: 'test-icon.png'
+    };
+
+    // Espera que o caso de uso lance um InvalidParamError com o parâmetro 'name'
+    await expect(usecase.execute(input)).rejects.toThrow(new InvalidParamError('name'));
+  });
+
+  test('Deve lançar um erro se a descrição do serviço estiver vazia', async () => {
+    const input: CreateServiceInputDto = {
+      name: 'Teste service',
+      description: '', // Descrição vazia
+      icon: 'test-icon.png'
+    };
+
+    // Espera que o caso de uso lance um InvalidParamError com o parâmetro 'description'
+    await expect(usecase.execute(input)).rejects.toThrow(new InvalidParamError('description'));
+  });
+
+  test('Deve lançar um erro se o ícone do serviço estiver vazio', async () => {
+    const input: CreateServiceInputDto = {
+      name: 'Teste service',
+      description: 'Este é um serviço de teste',
+      icon: '' // Ícone vazio
+    };
+    // Espera que o caso de uso lance um InvalidParamError com o parâmetro 'icon'
+    await expect(usecase.execute(input)).rejects.toThrow(new InvalidParamError('icon'));
+  });
+
+  test('Deve lançar um erro de servidor se ocorrer um erro ao salvar o serviço', async () => {
+    const input: CreateServiceInputDto = {
+      name: 'Teste service',
+      description: 'Este é um serviço de teste',
+      icon: 'test-icon.png'
+    };
+
+    // Simula uma falha no método save do gateway
+    mockServiceGateway.save.mockRejectedValueOnce(new ServerError());
+
+    // Espera que o caso de uso lance um ServerError
+    await expect(usecase.execute(input)).rejects.toThrow(ServerError);
   });
 });
--- src/domain/gateway/service.gateway.ts
+++ src/domain/gateway/service.gateway.ts
@@ -2,6 +2,7 @@ import { Service } from '../entities/service.entity';
 
 // Define a interface ServiceGateway para a persistência de dados
 export interface ServiceGateway {
-  // Método que deve ser implementado para salvar um serviço
+  // Método que deve ser implementado para salvar e listar serviço
   save(service: Service): Promise<void>;
+  list(): Promise<Service[]>;
 }
