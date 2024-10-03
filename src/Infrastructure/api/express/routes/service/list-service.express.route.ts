0095b026514627db2932ee99794c45043a83aaba
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-10-03 20:54:53 -0300

    feat: Implements service listing route with use case and integration with Express

---
+++ src/Infrastructure/api/express/routes/service/list-service.express.route.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from 'express';
+import {
+  ListServiceOutputDto,
+  ListServiceUsecase
+} from '../../../../../application/service/list-service/list-service.usecase';
+import { HttpMethod, Route } from '../route';
+
+export type ListServiceResponseDto = {
+  services: {
+    id: string;
+    name: string;
+    description: string;
+    icon: string;
+  }[];
+};
+
+export class ListServiceRoute implements Route {
+  private constructor(
+    private readonly path: string,
+    private readonly method: HttpMethod,
+    private readonly listServiceService: ListServiceUsecase
+  ) {}
+
+  // Construtor privado para controlar a criação da instância
+  public static create(listServiceService: ListServiceUsecase) {
+    return new ListServiceRoute('/services', HttpMethod.GET, listServiceService);
+  }
+
+  // Função que retorna o handler da rota, que é a função executada ao receber uma requisição
+  public getHandler(): (request: Request, response: Response) => Promise<void> {
+    return async (request: Request, response: Response) => {
+      // Executa o caso de uso de listagem de serviços
+      const output = await this.listServiceService.execute();
+
+      // Prepara a resposta com os dados dos serviços listados
+      const responseBody = this.present(output);
+
+      // Envia a resposta com status 200 (OK) e o corpo contendo os serviços listados
+      response.status(200).json(responseBody).send();
+    };
+  }
+
+  getMethod(): HttpMethod {
+    return this.method;
+  }
+
+  getPath(): string {
+    return this.path;
+  }
+
+  // Método para formatar a resposta de saída
+  private present(input: ListServiceOutputDto): ListServiceResponseDto {
+    const response: ListServiceResponseDto = {
+      services: input.services.map((s) => ({
+        id: s.id,
+        name: s.name,
+        description: s.description,
+        icon: s.icon
+      }))
+    };
+
+    return response;
+  }
+}
