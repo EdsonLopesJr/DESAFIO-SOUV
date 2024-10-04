d48f9e83604fdd4eb8e973b3498270a5a1832534
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-10-04 11:47:01 -0300

    feat: add tablee testimonials migrate

---
+++ prisma/migrations/20241004144102_add_new_table_testimonials/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "testimonials" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+
+    CONSTRAINT "testimonials_pkey" PRIMARY KEY ("id")
+);
