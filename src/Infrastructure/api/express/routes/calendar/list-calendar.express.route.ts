44d727f78ef5965e8ebc2e7ef9e0c16bdd4b6122
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-10-03 23:09:54 -0300

    feat: Implements calendar listing route with use case and integration with Express

---
+++ src/Infrastructure/api/express/routes/calendar/list-calendar.express.route.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from 'express';
+import {
+  ListCalendarOutputDto,
+  ListCalendarUsecase
+} from '../../../../../application/calendar/list-calendar/list-calendar.usecase';
+import { HttpMethod, Route } from '../route';
+
+// Define a estrutura do DTO de resposta para a listagem de calendários.
+export type ListCalendarResponseDto = {
+  calendars: {
+    id: string;
+    name: string;
+    phone: string;
+    services: {
+      id: string;
+      name: string;
+    }[];
+    timestamp: Date;
+  }[];
+};
+
+// Classe que implementa a rota para listar calendários.
+export class ListCalendarRoute implements Route {
+  private constructor(
+    private readonly path: string,
+    private readonly method: HttpMethod,
+    private readonly listCalendarService: ListCalendarUsecase
+  ) {}
+
+  public static create(listCalendarService: ListCalendarUsecase) {
+    return new ListCalendarRoute('/calendars', HttpMethod.GET, listCalendarService);
+  }
+
+  public getMethod(): HttpMethod {
+    return this.method;
+  }
+
+  public getPath(): string {
+    return this.path;
+  }
+
+  // Método que retorna o manipulador da rota.
+  public getHandler(): (request: Request, response: Response) => Promise<void> {
+    return async (request: Request, response: Response) => {
+      // Executa o caso de uso de listagem de calendários e obtém a saída.
+      const output = await this.listCalendarService.execute();
+
+      const responseBody = this.present(output);
+
+      // Retorna a resposta com status 200 e o corpo da resposta.
+      response.status(200).json(responseBody).send();
+    };
+  }
+
+  // Método para formatar a resposta de saída.
+  private present(input: ListCalendarOutputDto): ListCalendarResponseDto {
+    const response: ListCalendarResponseDto = {
+      calendars: input.calendars.map((calendar) => ({
+        id: calendar.id,
+        name: calendar.name,
+        phone: calendar.phone,
+        services: calendar.services.map((service) => ({
+          id: service.id,
+          name: service.name
+        })),
+        timestamp: calendar.timestamp
+      }))
+    };
+
+    return response;
+  }
+}
