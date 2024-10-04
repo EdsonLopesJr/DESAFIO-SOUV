fd088570093bd3128106f144fe70d179a04155ec
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-10-03 23:12:34 -0300

    feat: add calendar routes express

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,41 @@
+import { CreateCalendarUsecase } from './application/calendar/create-calendar/create-calendar.usecase';
+import { ListCalendarUsecase } from './application/calendar/list-calendar/list-calendar.usecase';
 import { CreateServiceUsecase } from './application/service/create-service/create-service.usecase';
 import { ListServiceUsecase } from './application/service/list-service/list-service.usecase';
 import { ApiExpress } from './Infrastructure/api/express/api.express';
+import { CreateCalendarRoute } from './Infrastructure/api/express/routes/calendar/create-calendar.express.route';
+import { ListCalendarRoute } from './Infrastructure/api/express/routes/calendar/list-calendar.express.route';
 import { CreateServiceRoute } from './Infrastructure/api/express/routes/service/create-service.express.route';
 import { ListServiceRoute } from './Infrastructure/api/express/routes/service/list-service.express.route';
+import { CalendarRepository } from './Infrastructure/repositories/calendar/calendar.repository.prisma';
 import { ServiceRepositoryPrisma } from './Infrastructure/repositories/service/service.repository.prisma';
 import { prisma } from './package/prisma/prisma';
 
 function main() {
-  const aRepository = ServiceRepositoryPrisma.create(prisma);
-
   // Services
-  const createServiceUsecase = CreateServiceUsecase.create(aRepository);
-  const listServiceUsecase = ListServiceUsecase.create(aRepository);
+  const aServiceRepository = ServiceRepositoryPrisma.create(prisma);
+
+  const createServiceUsecase = CreateServiceUsecase.create(aServiceRepository);
+  const listServiceUsecase = ListServiceUsecase.create(aServiceRepository);
 
   // Cria as rotas para criar e listar serviços
   const createServiceRoute = CreateServiceRoute.create(createServiceUsecase);
   const listServiceRoute = ListServiceRoute.create(listServiceUsecase);
 
+  // Calendars
+
+  const aCalendarRepository = CalendarRepository.create(prisma);
+
+  const createCalendarUsecase = CreateCalendarUsecase.create(aCalendarRepository, aServiceRepository);
+  const listCalendarUsecase = ListCalendarUsecase.create(aCalendarRepository);
+
+  // Cria as rotas para criar e listar Calendar
+  const createCalendarRoute = CreateCalendarRoute.create(createCalendarUsecase);
+  const listeCalendarRoute = ListCalendarRoute.create(listCalendarUsecase);
+
   // Cria a API passando as rotas criadas
   const port = 3333;
-  const api = ApiExpress.create([createServiceRoute, listServiceRoute]);
+  const api = ApiExpress.create([createServiceRoute, listServiceRoute, createCalendarRoute, listeCalendarRoute]);
 
   // Inicia o servidor na porta definida
   api.start(port);
