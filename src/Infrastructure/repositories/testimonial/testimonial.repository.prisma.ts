7f46f3993da775dcf14b0d2210c7abec17edce85
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-10-04 11:47:29 -0300

    feat: implementsd repository testimonials prisma

---
+++ src/Infrastructure/repositories/testimonial/testimonial.repository.prisma.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client';
+import { TestimonialGateway } from '../../../domain/testimonial/gateway/testimonial.gateway';
+import { Testimonial } from '../../../domain/testimonial/entities/testimonial.entity';
+
+export class TestimonialRepositoryPrisma implements TestimonialGateway {
+  private constructor(private readonly prismaClient: PrismaClient) {}
+
+  public static create(prismaClient: PrismaClient) {
+    return new TestimonialRepositoryPrisma(prismaClient);
+  }
+
+  public async save(testimonial: Testimonial): Promise<void> {
+    const data = {
+      id: testimonial.id,
+      name: testimonial.name,
+      message: testimonial.message
+    };
+
+    await this.prismaClient.testimonial.create({ data });
+  }
+
+  public async list(): Promise<Testimonial[]> {
+    const testimonials = await this.prismaClient.testimonial.findMany();
+
+    const testimonialList = testimonials.map((t) => {
+      const testimonial = Testimonial.with({
+        id: t.id,
+        name: t.name,
+        message: t.message
+      });
+
+      return testimonial;
+    });
+
+    return testimonialList;
+  }
+}
