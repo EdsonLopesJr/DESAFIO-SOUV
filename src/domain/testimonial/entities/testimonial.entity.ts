2f03523c756e02bdb3831f52a8ec04d2db9adc4e
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-10-04 10:45:21 -0300

    feat: add handle error params

--- src/domain/testimonial/entities/testimonial.entity.ts
+++ src/domain/testimonial/entities/testimonial.entity.ts
@@ -1,3 +1,5 @@
+import { InvalidParamError } from '../../service/exceptions/invalid-params-error';
+
 export type TestimonialProps = {
   id: string;
   name: string;
@@ -8,6 +10,15 @@ export class Testimonial {
   private constructor(private props: TestimonialProps) {}
 
   public static create(name: string, message: string) {
+    const params = { name, message }; // Agrupa os parâmetros em um objeto
+
+    // Verifica se cada parâmetro é válido
+    for (const [key, value] of Object.entries(params)) {
+      if (!value) {
+        throw new InvalidParamError(key.charAt(0) + key.slice(1)); // Lança o erro se o parâmetro for inválido
+      }
+    }
+
     return new Testimonial({
       id: crypto.randomUUID().toString(),
       name,
