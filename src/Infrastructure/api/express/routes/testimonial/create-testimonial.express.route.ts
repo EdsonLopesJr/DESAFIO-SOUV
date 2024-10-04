b108ab9a39e203a90666ad68b4bca339149f99e0
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-10-04 11:46:14 -0300

    feat: implements  use case testimonials create

---
+++ src/Infrastructure/api/express/routes/testimonial/create-testimonial.express.route.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from 'express';
+import {
+  CreateTestimonialInputDto,
+  CreateTestimonialUsecase
+} from '../../../../../application/testimonial/create-testimonial/create-testimonial.usecase';
+import { HttpMethod, Route } from '../route';
+import { InvalidParamError } from '../../../../../domain/service/exceptions/invalid-params-error';
+import { ServerError } from '../../../../../application/exception/server-error';
+
+export type CreateTestimonialResponseDto = {
+  id: string;
+};
+
+export class CreateTestimonialRoute implements Route {
+  private constructor(
+    private readonly path: string,
+    private readonly method: HttpMethod,
+    private readonly createTestimonialService: CreateTestimonialUsecase
+  ) {}
+
+  public static create(createTestimonialService: CreateTestimonialUsecase) {
+    return new CreateTestimonialRoute('/testimonial', HttpMethod.POST, createTestimonialService);
+  }
+
+  public getMethod(): HttpMethod {
+    return this.method;
+  }
+
+  public getPath(): string {
+    return this.path;
+  }
+
+  public getHandler(): (request: Request, response: Response) => Promise<void> {
+    return async (request: Request, response: Response) => {
+      try {
+        const { name, message } = request.body;
+
+        const input: CreateTestimonialInputDto = {
+          name,
+          message
+        };
+
+        const output: CreateTestimonialResponseDto = await this.createTestimonialService.execute(input);
+
+        const responseBody = this.present(output);
+
+        response.status(201).json(responseBody).send();
+      } catch (error) {
+        if (error instanceof InvalidParamError) {
+          response.status(400).json({
+            message: error.message
+          });
+        } else if (error instanceof ServerError) {
+          response.status(500).json({
+            message: error.message
+          });
+        }
+      }
+    };
+  }
+  private present(input: CreateTestimonialResponseDto): CreateTestimonialResponseDto {
+    const response = { id: input.id };
+    return response;
+  }
+}
---
+++ src/Infrastructure/api/express/routes/testimonial/list-testimonial.express.ts
@@ -0,0 +1,75 @@
+import { ServerError } from '../../../../../application/exception/server-error';
+import {
+  ListTestimonialOutputDto,
+  ListTestimonialUsecase
+} from '../../../../../application/testimonial/list-testimonial/list-testimonial.usecase';
+import { HttpMethod, Route } from '../route';
+import { Request, Response } from 'express';
+
+export type ListTestimonialResponseDto = {
+  testimonials: {
+    id: string;
+    name: string;
+    message: string;
+  }[];
+};
+
+export class ListTestimonialRoute implements Route {
+  private constructor(
+    private readonly path: string,
+    private readonly method: HttpMethod,
+    private readonly listTestimonialTestimonial: ListTestimonialUsecase
+  ) {}
+
+  // Construtor privado para controlar a criação da instância
+  public static create(listTestimonialTestimonial: ListTestimonialUsecase) {
+    return new ListTestimonialRoute('/testimonials', HttpMethod.GET, listTestimonialTestimonial);
+  }
+
+  // Função que retorna o handler da rota, que é a função executada ao receber uma requisição
+  public getHandler(): (request: Request, response: Response) => Promise<void> {
+    return async (request: Request, response: Response) => {
+      try {
+        // Executa o caso de uso de listagem de depoimentos
+        const output = await this.listTestimonialTestimonial.execute();
+
+        // Prepara a resposta com os dados dos depoimentos listados
+        const responseBody = this.present(output);
+
+        // Envia a resposta com status 200 (OK) e o corpo contendo os depoimentos listados
+        response.status(200).json(responseBody).send();
+      } catch (error) {
+        if (error instanceof ServerError) {
+          response.status(500).json({
+            message: error.message
+          });
+        } else {
+          response.status(500).json({
+            message: 'Unexpected error occurred'
+          });
+        }
+      }
+    };
+  }
+
+  public getMethod(): HttpMethod {
+    return this.method;
+  }
+
+  public getPath(): string {
+    return this.path;
+  }
+
+  // Método para formatar a resposta de saída
+  private present(input: ListTestimonialOutputDto): ListTestimonialResponseDto {
+    const response: ListTestimonialResponseDto = {
+      testimonials: input.testimonials.map((t) => ({
+        id: t.id,
+        name: t.name,
+        message: t.message
+      }))
+    };
+
+    return response;
+  }
+}
